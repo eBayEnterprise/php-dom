d98d6f0cb8ec79b97035da8c6ae1c1c2adeb7daa
Michael Phang <phangm@trueaction.com>
2013-05-30 12:52:54 -0400

    fixed indentation.

--- src/app/code/local/TrueAction/Eb2c/Core/Model/TrueDom/Element.php
+++ src/app/code/local/TrueAction/Eb2c/Core/Model/TrueDom/Element.php
@@ -1,14 +1,14 @@
 <?php
 class TrueAction_Eb2c_Core_Model_TrueDom_Element extends DOMElement {
-  /**
-   * Create and append a child to this element.
-   *
-   * @param string $name The name of the element to create.
-   * @param string|DOMNode $val The child node to append to the created element.
-   * @param array $attrs Array of attribute names and values to append to the created element.
-   * @example $ex1 = $tde->createChild('foo', 'bar', array('fizzy'=>'wizzy')) -> "<foo fizzy='wizzy'>bar</foo>"
-   * @example $tde->createChild('xyzzy', $ex1) -> "<xyzzy><foo fizzy='wizzy'>bar</foo></xyzzy>"
-   */
+	/**
+	 * Create and append a child to this element.
+	 *
+	 * @param string $name The name of the element to create.
+	 * @param string|DOMNode $val The child node to append to the created element.
+	 * @param array $attrs Array of attribute names and values to append to the created element.
+	 * @example $ex1 = $tde->createChild('foo', 'bar', array('fizzy'=>'wizzy')) -> "<foo fizzy='wizzy'>bar</foo>"
+	 * @example $tde->createChild('xyzzy', $ex1) -> "<xyzzy><foo fizzy='wizzy'>bar</foo></xyzzy>"
+	 */
 	public function createChild($name, $val, array $attrs = null) {
 		$el = $this->appendChild(new TrueAction_Eb2c_Core_Model_TrueDom_Element($name, $val));
 		if (!is_null($attrs)) {
