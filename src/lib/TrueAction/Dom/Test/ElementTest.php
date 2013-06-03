f6b39f05b30553fceb27950d514bc62e5b39ea7e
Michael Phang <phangm@trueaction.com>
2013-06-03 12:33:51 -0400

    redid the tests for the element class.

--- src/lib/TrueAction/Dom/Test/ElementTest.php
+++ src/lib/TrueAction/Dom/Test/ElementTest.php
@@ -1,34 +1,55 @@
 <?php
 class TrueAction_Dom_Test_ElementTest extends EcomDev_PHPUnit_Test_Case
 {
+	public function setUp()
+	{
+		$this->doc = new TrueAction_Dom_Document();
+		$this->root = $this->doc->appendChild(
+			$this->doc->createElement('testroot')
+		);
+	}
+
 	/**
 	 * @test
 	 */
-	public function testUsage()
+	public function testCreateChild()
 	{
-		$doc = new TrueAction_Dom_Document();
-		$root = $doc->appendChild(
-			$doc->createElement('testroot')
-		);
-		$child = $root->createChild(
+		$child = $this->root->createChild(
 			'testchild',
 			'testval',
 			array('ref'=>'1', 'foo'=>'baz', '_1234'=>'biz', 'id'=>'234')
 		);
-		$child->setIdAttribute('id', true);
-		$this->assertSame(1, count($doc->getElementsByTagName('testroot')));
-		$this->assertSame(1, count($doc->getElementsByTagName('testchild')));
-		$this->assertSame(
-			$child,
-			$doc->getElementById('234')
-		);
-		$childt2_0 = $child->createChild(
-			'child2',
+		$this->assertSame('testchild', $child->nodeName);
+		$this->assertSame('testval', $child->nodeValue);
+		$this->assertSame('1', $child->getAttribute('ref'));
+		$this->assertSame(1, count($this->doc->getElementsByTagName('testroot')));
+		$this->assertSame(1, count($this->doc->getElementsByTagName('testchild')));
+	}
+
+	/**
+	 * @test
+	 */
+	public function testCreateChildSpecialChars()
+	{
+		$child1 = $this->root->createChild(
+			'child1',
 			'test<val'
 		);
-		$childt2_1 = $child->createChild(
+		$this->assertSame('test<val', $child1->textContent);
+		$child2 = $this->root->createChild(
 			'child2',
 			'test&val'
 		);
+		$this->assertSame('test&val', $child2->textContent);
+	}
+
+	/**
+	 * @test
+	 */
+	public function testSetAttribute()
+	{
+		$this->root->setAttribute('foo', '234', true);
+		$this->assertTrue($this->root->getAttributeNode('foo')->isId());
+		$this->assertSame($this->root, $this->doc->getElementById('234'));
 	}
 }
